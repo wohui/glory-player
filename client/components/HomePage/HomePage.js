7dbcbebb21ae7530c377a6c439e054b8261d343c
hui <907569450@qq.com>
2018-06-06 22:42:19 +0800

    更新 glory推荐歌单部分

--- client/components/HomePage/HomePage.js
+++ client/components/HomePage/HomePage.js
@@ -65,84 +65,73 @@ export default class HomePage extends React.Component {
                         </div>
                     </Layout.Col>
                 </Layout.Row>
+
                 <div className="songList">
-                    <p>Glory推荐</p>
+                    <p className="tuijian-title">Glory推荐</p>
                     <div className="demo-3 medium">
-                        <Carousel autoplay={false} arrow="always">
+                        <Carousel autoplay={false} arrow="always"  className="">
                             {
                                 [1, 2, 3].map((item, index) => {
                                     return (
                                         <Carousel.Item key={index}>
                                             <div className="listContent">
-                                                <Layout.Row>
-                                                    <Layout.Col span={ 4 } offset={ 0 } >
-                                                        <Card bodyStyle={{ padding: 0 }} className="image">
+                                                <Layout.Row  gutter="20" >
+                                                    <Layout.Col span={ 6 } offset={ 0 } >
+                                                        <Card bodyStyle={{ padding: 0 }} className="card">
                                                             <img src={this.state.songListImages[0]} className="image"></img>
-                                                            <div style={{ padding: 14 }}>
-                                                                <span>好吃的汉堡</span>
-                                                                <div className="bottom clearfix">
-                                                                    <time className="time">2016-10-21 16:19</time>
-                                                                    <Button type="text" className="button">播放</Button>
+                                                            <div style={{ padding: 5 }}>
+                                                                <div className="card-info">
+                                                                    <Layout.Row>
+                                                                        <Layout.Col span="24"><span className="name">你还不知道有没有这么的</span></Layout.Col>
+                                                                    </Layout.Row>
+                                                                    <Layout.Row>
+                                                                        <Layout.Col span="24"> <span type="text" className="">播放量:50.22万</span></Layout.Col>
+                                                                    </Layout.Row>
                                                                 </div>
                                                             </div>
                                                         </Card>
                                                     </Layout.Col>
-                                                    <Layout.Col span={ 4 } offset={ 0 }>
-                                                        <Card bodyStyle={{ padding: 0 }}>
+                                                    <Layout.Col span={ 6 } offset={ 0 }>
+                                                        <Card bodyStyle={{ padding: 0 }} className="card">
                                                             <img src={this.state.songListImages[1]} className="image"></img>
-                                                            <div style={{ padding: 14 }}>
-                                                                <span>好吃的汉堡</span>
-                                                                <div className="bottom clearfix">
-                                                                    <time className="time">2016-10-21 16:19</time>
-                                                                    <Button type="text" className="button">播放</Button>
+                                                            <div style={{ padding: 5 }}>
+                                                                <div className="card-info">
+                                                                    <Layout.Row>
+                                                                        <Layout.Col span="24"><span className="name">你还不知道有没有这么的</span></Layout.Col>
+                                                                    </Layout.Row>
+                                                                    <Layout.Row>
+                                                                        <Layout.Col span="24"> <span type="text" className="">播放量:50.22万</span></Layout.Col>
+                                                                    </Layout.Row>
                                                                 </div>
                                                             </div>
                                                         </Card>
                                                     </Layout.Col>
-                                                    <Layout.Col span={ 4 } offset={ 0 }>
-                                                        <Card bodyStyle={{ padding: 0 }}>
+                                                    <Layout.Col span={ 6 } offset={ 0 }>
+                                                        <Card bodyStyle={{ padding: 0 }} className="card">
                                                             <img src={this.state.songListImages[2]} className="image"></img>
-                                                            <div style={{ padding: 14 }}>
-                                                                <span>好吃的汉堡</span>
-                                                                <div className="bottom clearfix">
-                                                                    <time className="time">2016-10-21 16:19</time>
-                                                                    <Button type="text" className="button">播放</Button>
+                                                            <div style={{ padding: 5 }}>
+                                                                <div className="card-info">
+                                                                    <Layout.Row>
+                                                                        <Layout.Col span="24"><span className="name">你还不知道有没有这么的</span></Layout.Col>
+                                                                    </Layout.Row>
+                                                                    <Layout.Row>
+                                                                        <Layout.Col span="24"> <span type="text" className="">播放量:50.22万</span></Layout.Col>
+                                                                    </Layout.Row>
                                                                 </div>
                                                             </div>
                                                         </Card>
                                                     </Layout.Col>
-                                                    <Layout.Col span={ 4 } offset={ 0 }>
-                                                        <Card bodyStyle={{ padding: 0 }}>
+                                                    <Layout.Col span={ 6 } offset={ 0 }>
+                                                        <Card bodyStyle={{ padding: 0 }} className="card">
                                                             <img src={this.state.songListImages[1]} className="image"></img>
-                                                            <div style={{ padding: 14 }}>
-                                                                <span>好吃的汉堡</span>
-                                                                <div className="bottom clearfix">
-                                                                    <time className="time">2016-10-21 16:19</time>
-                                                                    <Button type="text" className="button">播放</Button>
-                                                                </div>
-                                                            </div>
-                                                        </Card>
-                                                    </Layout.Col>
-                                                    <Layout.Col span={ 4 } offset={ 0 }>
-                                                        <Card bodyStyle={{ padding: 0 }}>
-                                                            <img  src={this.state.songListImages[0]} className="image"></img>
-                                                            <div style={{ padding: 14 }}>
-                                                                <span>好吃的汉堡</span>
-                                                                <div className="bottom clearfix">
-                                                                    <time className="time">2016-10-21 16:19</time>
-                                                                    <Button type="text" className="button">播放</Button>
-                                                                </div>
-                                                            </div>
-                                                        </Card>
-                                                    </Layout.Col>
-                                                    <Layout.Col span={ 4 } offset={ 0 }>
-                                                        <Card bodyStyle={{ padding: 0 }}>
-                                                            <img src={this.state.songListImages[0]} className="image"></img>
-                                                            <div style={{ padding: 14 }}>
-                                                                <span>好吃的汉堡</span>
-                                                                <div className="bottom clearfix">
-                                                                    <time className="time">2016-10-21 16:19</time>
-                                                                    <Button type="text" className="button">播放</Button>
+                                                            <div style={{ padding: 5 }}>
+                                                                <div className="card-info">
+                                                                    <Layout.Row>
+                                                                        <Layout.Col span="24"><span className="name">你还不知道有没有这么的</span></Layout.Col>
+                                                                    </Layout.Row>
+                                                                    <Layout.Row>
+                                                                        <Layout.Col span="24"> <span type="text" className="">播放量:50.22万</span></Layout.Col>
+                                                                    </Layout.Row>
                                                                 </div>
                                                             </div>
                                                         </Card>
--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,7 @@
 const  testConfig ='./config_test.js'
 
 var config = null;
-
+//todo 根据不同环境加载不同配置
 if (1==1){
     config = require(testConfig)
 }
